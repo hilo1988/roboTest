23d901c971d24c768da39522365a8ff9dba11415
hilo <yoidukigembu@gmail.com>
2024-06-27 13:42:47 +0900

    a

---
+++ app/src/test/java/com/example/robotest/ExampleUnitTest.kt
@@ -0,0 +1,84 @@
+package com.example.robotest
+
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.RoborazziOptions
+import com.github.takahirom.roborazzi.RoborazziRule
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@Config(
+    sdk = [30],
+    qualifiers = RobolectricDeviceQualifiers.Pixel7,
+)
+@RunWith(RobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+class ExampleUnitTest {
+    @get:Rule
+    val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> =
+        createAndroidComposeRule<ComponentActivity>()
+
+
+    @OptIn(ExperimentalRoborazziApi::class)
+    @get:Rule
+    val roborazziRule = RoborazziRule(
+        composeRule = composeTestRule,
+        captureRoot = composeTestRule.onRoot(),
+        options = RoborazziRule.Options(
+            captureType = RoborazziRule.CaptureType.LastImage(),
+            roborazziOptions = RoborazziOptions(
+                recordOptions = RoborazziOptions.RecordOptions(
+                    resizeScale = 1.0,
+                    applyDeviceCrop = false,
+                    pixelBitConfig = RoborazziOptions.PixelBitConfig.Argb8888,
+                ),
+            ),
+        ),
+    )
+
+
+    @Test
+    fun circleTest() {
+        composeTestRule.setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testTag("aaa"),
+                contentAlignment = Alignment.Center
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(120.dp)
+                        .background(color = Color.Companion.DarkGray, shape = CircleShape)
+                )
+            }
+        }
+    }
+}
